4600f2e38284975573a9286f08f09568fac554ea
Jimbo Jones <128925976+jsteele15@users.noreply.github.com>
2025-04-20 15:02:29 +0100

    Update machine_gun_post.gd
    - Added a bool to the machine gun post that works out if the posts been destroyed yet or not
    - Added a function to the machine guy post that will play animation and such

--- Terrain Objects/machine_gun_post.gd
+++ Terrain Objects/machine_gun_post.gd
@@ -1,5 +1,12 @@
 extends Sprite2D
 
-"""
-This will contain the info to animate its destroyed state
-"""
+
+var destroyed : bool = false #used to tell if the machine gun post can be destroyed or is still firing  
+
+func blow_up() -> void:
+	"""
+	Right now this will just change the colour of the machine gun post,
+	but we'll add special affects and stuff later, really tart it up
+	"""
+	self.modulate = Color(0.45, 0.45, 0.45, 1.00)
+	destroyed = true
