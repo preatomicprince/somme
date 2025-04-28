5f358020671ad4084670e087445c6ba196949bd5
Jimbo Jones <128925976+jsteele15@users.noreply.github.com>
2025-04-28 16:47:09 +0100

    main menu
    -tarted up the main menu

--- Menus and UI/main_menu.gd
+++ Menus and UI/main_menu.gd
@@ -4,11 +4,11 @@ extends Node2D
 @onready var main_game = "res://Game/game.tscn"
 @onready var credits = "res://Menus and UI/credits.tscn"
 
-func _on_training_button_down() -> void:
-	print("basic training")
+	
 
-func _on_main_game_button_down() -> void:
+func _on_training_button_up() -> void:
 	get_tree().change_scene_to_file(main_game)
 
-func _on_credits_button_down() -> void:
+
+func _on_credits_button_up() -> void:
 	get_tree().change_scene_to_file(credits)
